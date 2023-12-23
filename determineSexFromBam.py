141a3afbfb4e28ccdecb9fe62fbc1e7110d2bccd
Adriano De Marino, PhD <adriano.demarino@gmail.com>
2023-12-22 22:13:24 -0800

    Update determineSexFromBam.py new version
    ### Key Changes:
    
    - **Dynamic Threshold**: The threshold for determining sex has been set to a ratio of 0.1 for Y chromosome reads to autosomal reads. This value can be adjusted based on your dataset's characteristics.
    
    - **Error Handling**: Added more detailed error handling and messages.
    
    - **Parameterization of Filters**: Users can now pass a comma-separated list of filters as a second argument. If no filters are provided, a default list is used.
    
    - **Flexibility in Reference Genome**: The script now handles different chromosome naming conventions by checking if chromosome names are numeric for autosomes and looking for 'Y' in the chromosome name for Y chromosome reads.
    
    - **Sex Determination Using X/Y Coverage Ratio**: The `calculate_sex` function now calculates the sum of reads for the X chromosome and the Y chromosome separately. It then computes the ratio of X to Y coverage. If this ratio is greater than 4, it classifies the sample as female; otherwise, it classifies it as male.
    
    - **Handling Zero Y Reads**: To avoid division by zero, if the Y reads are zero, the function automatically classifies the sample as female.

--- determineSexFromBam.py
+++ determineSexFromBam.py
@@ -6,41 +6,45 @@ import pandas as pd
   |             Determine Sex in Homo Sapiens              |
   +--------------------------------------------------------+
   |  Author:   Adriano De Marino                           |
-  |  Date:     April 2020                                  |
+  |  Date:     April 2020 v1, Dec 2023 v2                  |
   |  Contact:  adriano.demarino@gmail.com                  |
-  |  Version:  1.0.0                                       |
+  |  Version:  2.0.0                                       |
   +--------------------------------------------------------+
 '''
 
-for file in sys.argv[1:]:
-	
-	basename = os.path.basename(file).replace(".bam","")
-	
-	bashCommand = 'samtools idxstats {file} | \
-	grep -v "Un" | \
-	grep -v "random" | \
-	grep -v "alt" | \
-	grep -v "GL" | \
-	grep -v "NC" | \
-	grep -v "hs" | \
-	grep -v "*" | \
-	grep -v "fix" | \
-	grep -v "M" | \
-	sort -V | \
-	cut -f 1,3 > /tmp/{out}.sex'.format(file=file,out=basename)
+# Usage: python determineSexFromBam.py [bam_file] [optional: filters]
 
-	try:
-		os.system(bashCommand)
-		df = pd.read_csv("/tmp/{file}.sex".format(file=basename),sep="\t",header=None) 
-	except Exception as e:
-		"Are you using sorted bam file ?"
-		exit()
-	
-	autosom = int(df.loc[0:21,1].mean()) 
+def calculate_sex(df):
+    try:
+        x_reads = df.loc[df['chromosome'].str.contains('X', case=False), 'reads'].sum()
+        y_reads = df.loc[df['chromosome'].str.contains('Y', case=False), 'reads'].sum()
 
-	if df.loc[22,1]/autosom <= 0.8: 
-		print(basename,"\t","MALE",sep="") 
-	else: 
-		print(basename,"\t","FEMALE",sep="")
+        if y_reads == 0:  # Avoid division by zero
+            return 'FEMALE'
 
-exit()
+        ratio = x_reads / y_reads
+        return 'FEMALE' if ratio > 4 else 'MALE'
+    except Exception as e:
+        print("Error in calculating sex:", e)
+        sys.exit(1)
+
+def main():
+    for file in sys.argv[1:]:
+        basename = os.path.basename(file).replace(".bam", "")
+        filters = sys.argv[2].split(',') if len(sys.argv) > 2 else ["Un", "random", "alt", "GL", "NC", "hs", "*", "fix", "M"]
+
+        grep_command = ' | '.join([f'grep -v "{f}"' for f in filters])
+        bash_command = f'samtools idxstats {file} | {grep_command} | sort -V | cut -f 1,3 > /tmp/{basename}.sex'
+
+        try:
+            os.system(bash_command)
+            df = pd.read_csv(f"/tmp/{basename}.sex", sep="\t", names=["chromosome", "reads"])
+        except Exception as e:
+            print(f"Error processing {file}: {e}")
+            continue
+
+        sex = calculate_sex(df)
+        print(f"{basename}\t{sex}")
+
+if __name__ == "__main__":
+    main()
